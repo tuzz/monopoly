e4421a5be997a8cd72eea58d84d5eca34132e485
Chris Patuzzo <chris@patuzzo.co.uk>
2017-07-09 13:00:09 +0100

    Add taxes

---
+++ spec/monopoly/taxSpec.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const DescribedClass = lib("monopoly/tax");
+
+describe("Tax", () => {
+  it("has a name and amount", () => {
+    const tax = new DescribedClass({ name: "foo", amount: 123 });
+
+    expect(tax.name).toEqual("foo");
+    expect(tax.amount).toEqual(123);
+  });
+
+  it("pre-defines constants for standard monopoly taxes", () => {
+    const tax = DescribedClass.INCOME_TAX;
+
+    expect(tax.name).toEqual("Income Tax");
+    expect(tax.amount).toEqual(200);
+  });
+});

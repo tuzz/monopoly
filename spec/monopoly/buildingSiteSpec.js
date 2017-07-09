beacdf9e9b91d3dbb1a464ff74dcdb52698cffc8
Chris Patuzzo <chris@patuzzo.co.uk>
2017-07-09 12:35:49 +0100

    Add building sites
    For now, I’m going to use the UK version of
    Monopoly. Later, this could be extracted into a
    ‘Theme’ or ‘Variation’ to customise it.

---
+++ spec/monopoly/buildingSiteSpec.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const DescribedClass = lib("monopoly/buildingSite");
+
+describe("BuildingSite", () => {
+  it("has a name, price and colour", () => {
+    const site = new DescribedClass({ name: "foo", price: 123, colour: "red" });
+
+    expect(site.name).toEqual("foo");
+    expect(site.price).toEqual(123);
+    expect(site.colour).toEqual("red");
+  });
+
+  it("pre-defines constants for standard monopoly sites", () => {
+    const site = DescribedClass.TRAFALGAR_SQUARE;
+
+    expect(site.name).toEqual("Trafalgar Square");
+    expect(site.price).toEqual(240);
+    expect(site.colour).toEqual("red");
+  });
+});

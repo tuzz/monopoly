c4f6331f5a89f302939f50a19a1cfd1de533caf3
Chris Patuzzo <chris@patuzzo.co.uk>
2017-07-09 13:05:01 +0100

    Add a ‘GO’ singleton

---
+++ spec/monopoly/goSpec.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const DescribedClass = lib("monopoly/go");
+
+describe("Go", () => {
+  it("has a name and salary", () => {
+    const go = new DescribedClass({ name: "foo", salary: 123 });
+
+    expect(go.name).toEqual("foo");
+    expect(go.salary).toEqual(123);
+  });
+
+  it("pre-defines a singleton constant", () => {
+    const go = DescribedClass.SINGLETON;
+
+    expect(go.name).toEqual("GO");
+    expect(go.salary).toEqual(200);
+  });
+});
